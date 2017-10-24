87ae5f5a2160725b01a10ca8e0a2522ef2e394b4
Adrian Godong <adrian.godong@gmail.com>
2017-10-23 20:55:49 -0700

    Added GitHubHook.S3Dump sample code.

---
+++ samples/GitHubHook.S3Dump/Function.cs
@@ -0,0 +1,32 @@
+using System.Threading.Tasks;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using GitHubHook.S3Dump.Handlers;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+namespace GitHubHook.S3Dump
+{
+    public class Function
+    {
+        private readonly LambdaHandler lambdaHandler;
+
+        public async Task<APIGatewayProxyResponse> FunctionHandler(
+            APIGatewayProxyRequest input,
+            ILambdaContext context)
+        {
+            return await lambdaHandler.Handle(input, context);
+        }
+
+        public Function()
+        {
+            var eventHandlers = new EventHandlersRegistry();
+            eventHandlers.RegisterEventHandler<AnyToS3Handler>("ping");
+
+            lambdaHandler = new LambdaHandler(
+                new Authentication(),
+                eventHandlers,
+                new EventPayloadFactory());
+        }
+    }
+}
---
+++ samples/GitHubHook.S3Dump/Handlers/AnyToS3Handler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using GitHubHook.Models;
+
+namespace GitHubHook.S3Dump.Handlers
+{
+    internal class AnyToS3Handler : GitHubHook.Handlers.BaseEventHandler
+    {
+        private const string PingToS3BucketNameEnvironmentVariableName = "PINGTOS3_BUCKET_NAME";
+        private readonly string bucketName;
+
+        public AnyToS3Handler()
+        {
+            bucketName = Environment.GetEnvironmentVariable(PingToS3BucketNameEnvironmentVariableName);
+        }
+
+        public override async Task<string> HandleEvent(
+            APIGatewayProxyRequest request,
+            ILambdaContext context,
+            string deliveryId,
+            BaseEvent eventPayload)
+        {
+            var client = new Amazon.S3.AmazonS3Client();
+            var putObjectRequest = new Amazon.S3.Model.PutObjectRequest
+            {
+                BucketName = bucketName,
+                Key = deliveryId,
+                ContentBody = request.Body
+            };
+
+            await client.PutObjectAsync(putObjectRequest);
+
+            return $"Event payload uploaded to {bucketName}/{deliveryId}";
+        }
+    }
+}
