6192539fbae14b698091020d7550e4be69f1027b
Adrian Godong <adrian.godong@gmail.com>
2017-10-23 20:55:49 -0700

    Added LambdaHandler and tests.

---
+++ GitHubHook.Tests/LambdaHandlerTests.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using GitHubHook.Handlers;
+using GitHubHook.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace GitHubHook.Tests
+{
+    [TestClass]
+    public class LambdaHandlerTests
+    {
+
+        private Mock<IAuthentication> mockAuthentication;
+        private Mock<IEventHandlersRegistry> mockEventHandlers;
+        private Mock<IEventPayloadFactory> mockEventPayloadFactory;
+        private Mock<ILambdaContext> mockLambdaContext;
+        private Mock<ILambdaLogger> mockLambdaLogger;
+
+        private LambdaHandler lambdaHandler;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            mockAuthentication = new Mock<IAuthentication>();
+            mockEventHandlers = new Mock<IEventHandlersRegistry>();
+            mockEventPayloadFactory = new Mock<IEventPayloadFactory>();
+            mockLambdaContext = new Mock<ILambdaContext>();
+            mockLambdaLogger = new Mock<ILambdaLogger>();
+
+            mockLambdaContext
+                .Setup(mock => mock.Logger)
+                .Returns(mockLambdaLogger.Object);
+
+            lambdaHandler = new LambdaHandler(
+                mockAuthentication.Object,
+                mockEventHandlers.Object,
+                mockEventPayloadFactory.Object);
+        }
+
+        [DataTestMethod]
+        [DataRow(AuthenticationResult.HasSignatureButMissingSecretToken, 500)]
+        [DataRow(AuthenticationResult.InvalidSignature, 400)]
+        [DataRow(AuthenticationResult.InvalidPayload, 400)]
+        [DataRow(AuthenticationResult.MismatchedSignature, 401)]
+        public async Task Handle_Test1(AuthenticationResult authenticationResult, int statusCode)
+        {
+            // Arrange
+            mockAuthentication
+                .Setup(mock => mock.Authenticate(It.IsAny<APIGatewayProxyRequest>()))
+                .Returns(authenticationResult);
+
+            // Act
+            var result = await lambdaHandler.Handle(null, null);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(statusCode, result.StatusCode);
+        }
+
+        /// <summary>
+        /// + Authenticate returns AuthenticationResult.Success or AuthenticationResult.SignatureNotRequired
+        /// + Delivery ID is missing
+        /// 
+        /// - Returns 400
+        /// </summary>
+        [DataTestMethod]
+        [DataRow(AuthenticationResult.Success)]
+        [DataRow(AuthenticationResult.SignatureNotRequired)]
+        public async Task Handle_Test2(AuthenticationResult authenticationResult)
+        {
+            // Arrange
+            mockAuthentication
+                .Setup(mock => mock.Authenticate(It.IsAny<APIGatewayProxyRequest>()))
+                .Returns(authenticationResult);
+
+            var request = new APIGatewayProxyRequest
+            {
+                Headers = new Dictionary<string, string>()
+            };
+
+            // Act
+            var result = await lambdaHandler.Handle(request, mockLambdaContext.Object);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(400, result.StatusCode);
+        }
+
+        /// <summary>
+        /// + Authenticate returns AuthenticationResult.Success or AuthenticationResult.SignatureNotRequired
+        /// + Delivery ID exists
+        /// + Event ID is missing
+        /// 
+        /// - Returns 400
+        /// </summary>
+        [DataTestMethod]
+        [DataRow(AuthenticationResult.Success)]
+        [DataRow(AuthenticationResult.SignatureNotRequired)]
+        public async Task Handle_Test3(AuthenticationResult authenticationResult)
+        {
+            // Arrange
+            mockAuthentication
+                .Setup(mock => mock.Authenticate(It.IsAny<APIGatewayProxyRequest>()))
+                .Returns(authenticationResult);
+
+            var request = new APIGatewayProxyRequest
+            {
+                Headers = new Dictionary<string, string>()
+                {
+                    { LambdaHandler.XGitHubDeliveryHeaderKey, Guid.NewGuid().ToString("N") }
+                }
+            };
+
+            // Act
+            var result = await lambdaHandler.Handle(request, mockLambdaContext.Object);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(400, result.StatusCode);
+        }
+
+        [DataTestMethod]
+        [DataRow(AuthenticationResult.Success)]
+        [DataRow(AuthenticationResult.SignatureNotRequired)]
+        public async Task Handle_AllValid_ShouldReturnOkResponse(AuthenticationResult authenticationResult)
+        {
+            // Arrange
+            var body = Guid.NewGuid().ToString("N");
+
+            mockAuthentication
+                .Setup(mock => mock.Authenticate(It.IsAny<APIGatewayProxyRequest>()))
+                .Returns(authenticationResult);
+
+            mockEventHandlers
+                .Setup(mock => mock.GetEventHandlersOrDefault(It.IsAny<string>()))
+                .Returns(new List<BaseEventHandler>
+                {
+                    new DefaultHandler()
+                });
+
+            mockEventPayloadFactory
+                .Setup(mock => mock.CreateEventPayload(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(new Mock<BaseEvent>().Object);
+
+            var request = new APIGatewayProxyRequest
+            {
+                Headers = new Dictionary<string, string>
+                {
+                    { LambdaHandler.XGitHubDeliveryHeaderKey, Guid.NewGuid().ToString("N") },
+                    { LambdaHandler.XGitHubEventHeaderKey, Guid.NewGuid().ToString("N") }
+                },
+                Body = body
+            };
+
+            // Act
+            var response = await lambdaHandler.Handle(request, mockLambdaContext.Object);
+
+            // Assert
+            Assert.IsNotNull(response);
+            Assert.AreEqual(200, response.StatusCode);
+            Assert.AreEqual($"{new DefaultHandler()}:{body}", response.Body);
+        }
+
+    }
+}
---
+++ GitHubHook/LambdaHandler.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+
+namespace GitHubHook
+{
+    public sealed class LambdaHandler
+    {
+
+        internal const string XGitHubDeliveryHeaderKey = "X-GitHub-Delivery";
+        internal const string XGitHubEventHeaderKey = "X-GitHub-Event";
+
+        private readonly IAuthentication authentication;
+        private readonly IEventHandlersRegistry eventHandlers;
+        private readonly IEventPayloadFactory eventPayloadFactory;
+
+        public LambdaHandler(
+            IAuthentication authentication,
+            IEventHandlersRegistry eventHandlers,
+            IEventPayloadFactory eventPayloadFactory)
+        {
+            this.authentication = authentication;
+            this.eventHandlers = eventHandlers;
+            this.eventPayloadFactory = eventPayloadFactory;
+
+            // TODO: do we need validation for mismatch between event handlers and payload factories?
+        }
+
+        public async Task<APIGatewayProxyResponse> Handle(APIGatewayProxyRequest request, ILambdaContext context)
+        {
+            var authenticationResult = authentication.Authenticate(request);
+            switch (authenticationResult)
+            {
+                case AuthenticationResult.Success:
+                    context.Logger.Log("DEBUG: Request signature validated");
+                    break;
+                case AuthenticationResult.SignatureNotRequired:
+                    context.Logger.Log("WARNING: Request is not signed");
+                    break;
+                case AuthenticationResult.HasSignatureButMissingSecretToken:
+                    return new APIGatewayProxyResponse
+                    {
+                        StatusCode = 500,
+                        Body = "ERROR: Secret token not configured"
+                    };
+                case AuthenticationResult.InvalidSignature:
+                    return new APIGatewayProxyResponse
+                    {
+                        StatusCode = 400,
+                        Body = "ERROR: Invalid signature"
+                    };
+                case AuthenticationResult.InvalidPayload:
+                    return new APIGatewayProxyResponse
+                    {
+                        StatusCode = 400,
+                        Body = "ERROR: Invalid payload"
+                    };
+                case AuthenticationResult.MismatchedSignature:
+                    return new APIGatewayProxyResponse
+                    {
+                        StatusCode = 401,
+                        Body = "ERROR: Mismatched signature"
+                    };
+                default:
+                    throw new NotImplementedException($"Unknown authentication result '{authenticationResult}'");
+            }
+
+            if (!request.Headers.TryGetValue(XGitHubDeliveryHeaderKey, out var deliveryId))
+            {
+                return new APIGatewayProxyResponse
+                {
+                    StatusCode = 400,
+                    Body = $"Missing {XGitHubDeliveryHeaderKey} header"
+                };
+            }
+
+            if (!request.Headers.TryGetValue(XGitHubEventHeaderKey, out var eventId))
+            {
+                return new APIGatewayProxyResponse
+                {
+                    StatusCode = 400,
+                    Body = $"Missing {XGitHubEventHeaderKey} header"
+                };
+            }
+
+            context.Logger.Log($"DEBUG: Processing delivery {deliveryId} ({eventId})");
+
+            var handlers = eventHandlers.GetEventHandlersOrDefault(eventId);
+            var eventPayload = eventPayloadFactory.CreateEventPayload(eventId, request.Body);
+
+            var resultBuilder = new StringBuilder();
+            foreach (var handler in handlers)
+            {
+                var result = await handler.HandleEvent(request, context, deliveryId, eventPayload);
+                var resultString = $"{handler}:{result}";
+                resultBuilder.AppendLine(resultString);
+                context.Logger.Log($"DEBUG: {resultString}");
+            }
+
+            return new APIGatewayProxyResponse
+            {
+                StatusCode = 200,
+                Body = resultBuilder.ToString().TrimEnd()
+            };
+        }
+
+    }
+}
