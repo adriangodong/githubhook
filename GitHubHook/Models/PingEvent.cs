4950602469f3d97970785fafa95e2d880e3de449
Adrian Godong <adrian.godong@gmail.com>
2017-10-23 20:55:49 -0700

    Added ping event model.

---
+++ GitHubHook/Models/PingEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace GitHubHook.Models
+{
+    [GitHubEventType("ping")]
+    public sealed class PingEvent : BaseEvent
+    {
+        public string Zen { get; set; }
+        public double HookId { get; set; }
+    }
+}
