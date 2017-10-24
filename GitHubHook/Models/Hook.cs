a18eed9ac9ee479b71aff7bb28da2035c16acde9
Adrian Godong <adrian.godong@gmail.com>
2017-10-23 20:55:49 -0700

    Added ping event models.

---
+++ GitHubHook/Models/Hook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GitHubHook.Models
+{
+    public sealed class Hook : SnakeCaseNamedObject
+    {
+        public string Type { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Active { get; set; }
+        public string[] Events { get; set; }
+        public HookConfig Config { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public HookResponse LastResponse { get; set; }
+    }
+}
---
+++ GitHubHook/Models/HookResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace GitHubHook.Models
+{
+    public sealed class HookResponse : SnakeCaseNamedObject
+    {
+        public double? Code { get; set; }
+        public string Status { get; set; }
+        public string Message { get; set; }
+    }
+}
