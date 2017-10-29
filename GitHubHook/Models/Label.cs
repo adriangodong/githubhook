f9bb88bcc4ed171fd3c8bdeed84bce9d21aeedce
Adrian Godong <adrian.godong@gmail.com>
2017-10-29 10:23:04 -0700

    Added label event models.

---
+++ GitHubHook/Models/Label.cs
@@ -0,0 +1,10 @@
+﻿namespace GitHubHook.Models
+{
+    public class Label : SnakeCaseNamedObject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public bool Default { get; set; }
+    }
+}
