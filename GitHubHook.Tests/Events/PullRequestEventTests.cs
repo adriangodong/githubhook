f08878ecd83842ebd4cbe121974ee2f7fffc1d53
Adrian Godong <adrian.godong@gmail.com>
2017-11-05 09:58:06 -0800

    Split edited pull_request event models.

--- GitHubHook.Tests/Events/PullRequestEventTests.cs
+++ GitHubHook.Tests/Events/PullRequestEventTests.cs
@@ -29,12 +29,26 @@ namespace GitHubHook.Tests.Events
             var payload = resourceManager.GetString("AssignedPullRequestPayload.json");
 
             // Act
-            var pullRequestPayload = JsonConvert.DeserializeObject<PullRequestEvent>(payload);
+            var pullRequestPayload = JsonConvert.DeserializeObject<AssignedPullRequestEvent>(payload);
 
             // Assert
             Assert.IsNotNull(pullRequestPayload);
             Console.WriteLine(JsonConvert.SerializeObject(pullRequestPayload));
         }
+
+        [TestMethod]
+        public void Unlabeled_Deserialize_ShouldSucceed()
+        {
+            // Arrange
+            var payload = resourceManager.GetString("UnlabeledPullRequestPayload.json");
+
+        // Act
+        var pullRequestPayload = JsonConvert.DeserializeObject<LabeledPullRequestEvent>(payload);
+
+        // Assert
+        Assert.IsNotNull(pullRequestPayload);
+        Console.WriteLine(JsonConvert.SerializeObject(pullRequestPayload));
+    }
 
         [TestMethod]
         public void ReviewRequested_Deserialize_ShouldSucceed()
@@ -43,7 +57,7 @@ namespace GitHubHook.Tests.Events
             var payload = resourceManager.GetString("ReviewRequestedPullRequestPayload.json");
 
             // Act
-            var pullRequestPayload = JsonConvert.DeserializeObject<PullRequestEvent>(payload);
+            var pullRequestPayload = JsonConvert.DeserializeObject<ReviewRequestedPullRequestEvent>(payload);
 
             // Assert
             Assert.IsNotNull(pullRequestPayload);
@@ -57,7 +71,7 @@ namespace GitHubHook.Tests.Events
             var payload = resourceManager.GetString("EditedPullRequestPayload.json");
 
             // Act
-            var pullRequestPayload = JsonConvert.DeserializeObject<PullRequestEvent>(payload);
+            var pullRequestPayload = JsonConvert.DeserializeObject<EditedPullRequestEvent>(payload);
 
             // Assert
             Assert.IsNotNull(pullRequestPayload);
@@ -71,7 +85,7 @@ namespace GitHubHook.Tests.Events
             var payload = resourceManager.GetString("SynchronizePullRequestPayload.json");
 
             // Act
-            var pullRequestPayload = JsonConvert.DeserializeObject<PullRequestEvent>(payload);
+            var pullRequestPayload = JsonConvert.DeserializeObject<SynchronizePullRequestEvent>(payload);
 
             // Assert
             Assert.IsNotNull(pullRequestPayload);
--- GitHubHook/Events/PullRequestEvent.cs
+++ GitHubHook/Events/PullRequestEvent.cs
@@ -9,10 +9,38 @@ namespace GitHubHook.Events
         public PullRequestEventAction Action { get; set; }
         public int Number { get; set; }
         public PullRequest PullRequest { get; set; }
+    }
+
+    [GitHubEventType("pull_request", "edited")]
+    public class EditedPullRequestEvent : PullRequestEvent
+    {
         public PullRequestChanges Changes { get; set; }
+    }
+
+    [GitHubEventType("pull_request", "assigned")]
+    [GitHubEventType("pull_request", "unassigned")]
+    public class AssignedPullRequestEvent : PullRequestEvent
+    {
         public User Assignee { get; set; }
-        public User RequestedReviewer { get; set; }
+    }
+
+    [GitHubEventType("pull_request", "labeled")]
+    [GitHubEventType("pull_request", "unlabeled")]
+    public class LabeledPullRequestEvent : PullRequestEvent
+    {
         public Label Label { get; set; }
+    }
+
+    [GitHubEventType("pull_request", "review_requested")]
+    [GitHubEventType("pull_request", "review_request_removed")]
+    public class ReviewRequestedPullRequestEvent : PullRequestEvent
+    {
+        public User RequestedReviewer { get; set; }
+    }
+
+    [GitHubEventType("pull_request", "synchronize")]
+    public class SynchronizePullRequestEvent : PullRequestEvent
+    {
         public string Before { get; set; }
         public string After { get; set; }
     }
