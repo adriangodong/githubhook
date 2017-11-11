b307bbf518fb557d4315f80398c3eebbca3dd821
Adrian Godong <adrian.godong@gmail.com>
2017-11-11 14:49:15 -0800

    Added release event models.

---
+++ GitHubHook/Events/Release.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using GitHubHook.Helpers;
+using GitHubHook.Models;
+
+namespace GitHubHook.Events
+{
+    public class Release : SnakeCaseNamedObject
+    {
+        public string UploadUrl { get; set; }
+        public string HtmlUrl { get; set; }
+        public long Id { get; set; }
+        public string TagName { get; set; }
+        public string TargetCommitish { get; set; }
+        public string Name { get; set; }
+        public bool Draft { get; set; }
+        public User Author { get; set; }
+        public bool Prerelease { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime PublishedAt { get; set; }
+        public string[] Assets { get; set; }
+        public string Body { get; set; }
+    }
+}
---
+++ GitHubHook/Events/ReleaseEvent.cs
@@ -0,0 +1,10 @@
+﻿using GitHubHook.Helpers;
+
+namespace GitHubHook.Events
+{
+    [GitHubEventType("release")]
+    public class ReleaseEvent : BaseActionEvent<ReleaseEventAction>
+    {
+        public Release Release { get; set; }
+    }
+}
