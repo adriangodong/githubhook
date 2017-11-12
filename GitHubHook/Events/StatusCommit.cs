21dfc464539505b397edeee97ab23343c692da35
Adrian Godong <adrian.godong@gmail.com>
2017-11-11 21:37:16 -0800

    Added status event models.

---
+++ GitHubHook/Events/StatusCommit.cs
@@ -0,0 +1,15 @@
+﻿using GitHubHook.Helpers;
+using GitHubHook.Models;
+
+namespace GitHubHook.Events
+{
+    public class StatusCommit : SnakeCaseNamedObject
+    {
+        public string Sha { get; set; }
+        // TODO: public object Commit { get; set; }
+        public string HtmlUrl { get; set; }
+        public User Author { get; set; }
+        public User Committer { get; set; }
+        public CommitRef[] Parents { get; set; }
+    }
+}
